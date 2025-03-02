c8779ef4ae01d57868e93ffd9d1482ce712ba7fd
visualizetheory <visualizetheory@gmail.com>
2025-03-02 13:21:17 +0100

    refined butterfly_effect

--- butterfly_effect/visualize.py
+++ butterfly_effect/visualize.py
@@ -2,12 +2,10 @@ from config import *
 from numerics import *
 
 
-ax = Axes(x_range=(-2.0, 2.0), x_length=1.3*4.0, y_range=(-4.0, 1.0), y_length=1.3*5.0).shift(0.5*DOWN).set_opacity(0.3)
-
 # CUSTOM MOBJECTS
 
 class DoublePendelum(VGroup):
-    def __init__(self, theta, phi, **kwargs):
+    def __init__(self, theta, phi, color=WHITE):
         start_point = ax.c2p(0, 0)
         middle_point = ax.c2p(l * sin(theta), - l * cos(theta))
         end_point = ax.c2p(l * sin(theta) + l * sin(phi), - l * cos(theta) - l * cos(phi))
@@ -18,7 +16,8 @@ class DoublePendelum(VGroup):
         first_dot = Dot(middle_point)
         second_dot = Dot(end_point)
 
-        super().__init__(first_line, first_dot, second_line, second_dot, **kwargs)
+        super().__init__(first_line, first_dot, second_line, second_dot)
+        self.set_color(color)
 
 class MotionTracker(ParametricFunction):
     def __init__(self, theta, phi, t_end, **kwargs):
@@ -30,7 +29,31 @@ class MotionTracker(ParametricFunction):
             **kwargs
         )
 
+class DynamicDoublePendelum(VGroup):
+    def __init__(self, t, theta_0, phi_0, sim_time, color):
+        super().__init__()
+        self.compute_solution(theta_0, phi_0, sim_time)
+        self.add_pendelum(t, color)
+        self.add_tracker(t, color)
+    
+    def compute_solution(self, theta_0, phi_0, sim_time):
+        self.numerics = DoublePendelumNumerics(theta_0=theta_0, phi_0=phi_0, sim_time=sim_time)
+        self.theta = self.numerics.get_theta_solution()
+        self.phi = self.numerics.get_phi_solution()
 
+    def add_pendelum(self, t, color):
+        self.pendelum = DoublePendelum(self.theta(t.get_value()), self.phi(t.get_value()), color=color)
+        self.pendelum.add_updater(lambda mob: mob.become(
+            DoublePendelum(self.theta(t.get_value()), self.phi(t.get_value()), color=color)  
+        ))
+        self += self.pendelum
+
+    def add_tracker(self, t, color):
+        self.tracker = MotionTracker(self.theta, self.phi, t.get_value(), color=color)
+        self.tracker.add_updater(lambda mob: mob.become(
+            MotionTracker(self.theta, self.phi, t.get_value(), color=color)
+        ))
+        self += self.tracker
 
 # parameters
 sim_time = 32.0
@@ -46,45 +69,26 @@ second_color = "#2CFA02"
 
 title_color = "#1bd7ea"
 
-# numerics
-first_ode = DoublePendelumNumerics(theta_0=first_theta_0, phi_0=first_phi_0, sim_time=sim_time)
-first_theta = first_ode.get_theta_solution()
-first_phi = first_ode.get_phi_solution()
+# axes
+ax = Axes(x_range=(-2.0, 2.0), x_length=1.3*4.0, y_range=(-4.0, 1.0), y_length=1.3*5.0).shift(0.5*DOWN).set_opacity(0.3)
 
-second_ode = DoublePendelumNumerics(theta_0=second_theta_0, phi_0=second_phi_0, sim_time=sim_time)
-second_theta = second_ode.get_theta_solution()
-second_phi = second_ode.get_phi_solution()
-
-# updaters for pendelums
+# updater for pendelums
 t = ValueTracker(0.0)
 
-first_pendelum = DoublePendelum(first_theta(t.get_value()), first_phi(t.get_value())).set_color(first_color)
-first_pendelum_copy = first_pendelum.copy()
-first_pendelum.add_updater(lambda mob: mob.become(
-    DoublePendelum(first_theta(t.get_value()), first_phi(t.get_value())).set_color(first_color)    
-))
-first_tracker = MotionTracker(first_theta, first_phi, t.get_value(), color=first_color)
-first_tracker.add_updater(lambda mob: mob.become(
-    MotionTracker(first_theta, first_phi, t.get_value(), color=first_color)
-))
+# pendelums
+first_dynamic_pendelum = DynamicDoublePendelum(t, first_theta_0, first_phi_0, sim_time, first_color)
+second_dynamic_pendelum = DynamicDoublePendelum(t, second_theta_0, second_phi_0, sim_time, second_color)
 
-second_pendelum = DoublePendelum(second_theta(t.get_value()), second_phi(t.get_value())).set_color(second_color)
-second_pendelum.add_updater(lambda mob: mob.become(
-    DoublePendelum(second_theta(t.get_value()), second_phi(t.get_value())).set_color(second_color)
-))
-second_tracker = MotionTracker(second_theta, second_phi, t.get_value(), color=second_color)
-second_tracker.add_updater(lambda mob: mob.become(
-    MotionTracker(second_theta, second_phi, t.get_value(), color=second_color)
-))
+first_dynamic_pendelum_copy = first_dynamic_pendelum.copy()
 
+# title
 title = Tex(r"\textbf{$\mathbb{B}$", r"utterfly effect.}", height=0.7).move_to(5*UP)
 title[0].set_color(title_color)
 
 # SCENE
-
 class ButterflyEffectScene(Scene):
     def construct(self):
-        self.add(ax, first_pendelum, first_pendelum_copy, first_tracker, second_tracker, title)
-        self.play(ReplacementTransform(first_pendelum_copy, second_pendelum), run_time=1.5)
+        self.add(ax, first_dynamic_pendelum, first_dynamic_pendelum_copy, title)
+        self.play(ReplacementTransform(first_dynamic_pendelum_copy, second_dynamic_pendelum))
         self.play(t.animate.set_value(sim_time), run_time=sim_time, rate_func=linear)
-        self.wait()
+        self.wait()        
